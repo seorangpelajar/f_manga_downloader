71a4e25f378466223cfcb95e982e3b2fe49440f0
seorangpelajar1 <seorangpelajar1@gmail.com>
2015-08-04 15:06:54 +0700

    Adding not safe for children

--- index.php
+++ index.php
@@ -22,6 +22,7 @@
 			<div class="row">
 				<div class="col-md-12">
 					<h1 align="center">This program just for shit. Do what the fuck you want!</h1>
+					<h3 align="center">Not safe for children. R18+</h3>
 					<p><strong>Feature Included:</strong></p>
 					<ul>
 						<li>Best experience for reading fakku manga.</li>
